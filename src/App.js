f79903409198778b46791f6a2e0927c18d9680a9
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-11-20 19:49:14 -0500

    carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Carousel from "./Components/Carousel/CarouselEffect";
 function App() {
   return (
     <div>
       <Header />
+      <Carousel />
     </div>
   );
 }
