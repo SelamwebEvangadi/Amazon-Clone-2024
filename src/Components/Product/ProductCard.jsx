9d31cfaeb8294933d4e99659664360cd705ea530
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-11-23 19:18:25 -0500

    Routing for Product Detail Page addedd

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -2,28 +2,29 @@ import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "../Product/Product.module.css";
+import { Link } from "react-router-dom";
 
 function ProductCard({ product }) {
   const { image, title, id, rating, price } = product;
   return (
     <div className={`${classes.card__container}`}>
-      <a href="">
-        <img src={image} alt="" />
-        <div>
-          <h3>{title}</h3>
-          <div className={classes.rating}>
-            {/* {Rating} */}
-            <Rating value={rating.rate} precision={0.1} />
-            {/* count */}
-            <small>{rating.count}</small>
-          </div>
-          <div>
-            {/* {price} */}
-            <CurrencyFormat amount={price} />
-          </div>
-          <button className={classes.button}>add to cart</button>
+      <Link to={`products/${id}`}>
+        <img src={image} alt="" className={classes.img__container} />
+      </Link>
+      <div>
+        <h3>{title}</h3>
+        <div className={classes.rating}>
+          {/* {Rating} */}
+          <Rating value={rating?.rate} precision={0.1} />
+          {/* count */}
+          <small>{rating?.count}</small>
         </div>
-      </a>
+        <div>
+          {/* {price} */}
+          <CurrencyFormat amount={price} />
+        </div>
+        <button className={classes.button}>add to cart</button>
+      </div>
     </div>
   );
 }
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "../ProductDetail/Productdetail.module.css";
 import LayOut from "../../Components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/Product/ProductCard";
 
+const initialProductState = {
+  id: "",
+  title: "",
+  image: "",
+  rating: { rate: 0, count: 0 },
+  price: 0,
+};
 function ProductDetail() {
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.log("Error fetching product:", err);
+      });
+  }, [productId]);
   return (
     <LayOut>
-      <div>ProductDetail</div>;
+      <ProductCard product={product} />
     </LayOut>
   );
 }
