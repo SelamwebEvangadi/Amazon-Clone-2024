b1db6641986556cde461a5d8389cc8882c6795d7
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-11-26 09:53:19 -0500

    authentication functionality added

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import classes from "../Auth/Signup.module.css";
 import { Link } from "react-router-dom";
-
+import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import {DataContext} from "../../Components/DataProvider/DataProvider.js"
+import { Type } from "../../Utility/action.type.js";
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // console.log(password, email);
+
+  const [{user}, dispatch] = useContext(DataContext)
+  console.log(user);
+  const authHandler = async (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    if (e.target.name === "signin") {
+      // # firebase auth
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          // console.log(userInfo);
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user
+          })
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          // console.log(userInfo);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <section className={classes.login}>
       {/* {logo} */}
@@ -18,14 +59,31 @@ function Auth() {
         <form action="">
           <div>
             <label htmlFor="Email"> Email</label>
-            <input type="email" id="email" />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+            />
           </div>
 
-          <button className={classes.login__SignInButton}>Sign In</button>
+          <button
+            type="submit"
+            onClick={authHandler}
+            name="signin"
+            className={classes.login__SignInButton}
+          >
+            Sign In
+          </button>
         </form>
 
         {/* {agreement} */}
@@ -35,7 +93,12 @@ function Auth() {
           Interest-Based Ads Notice.
         </p>
         {/* {create account btn} */}
-        <button classname={classes.login__registerButton}>
+        <button
+          type="submit"
+          onClick={authHandler}
+          name="signup"
+          className={classes.login__registerButton}
+        >
           create your amazon account
         </button>
       </div>
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -2,6 +2,7 @@ import { Type } from "./action.type";
 
 export const initialState = {
   basket: [],
+  user: null,
 };
 
 export const reducer = (state, action) => {
@@ -29,21 +30,27 @@ export const reducer = (state, action) => {
         };
       }
 
-      case Type.REMOVE_FROM_BASKET:
-        const index = state.basket.findIndex(item=> item.id===action.id)
-      let newBasket = [...state.basket]
+    case Type.REMOVE_FROM_BASKET:
+      const index = state.basket.findIndex((item) => item.id === action.id);
+      let newBasket = [...state.basket];
 
-       if (index >= 0){
-        if(newBasket[index].amount > 1){
-            newBasket[index] = {...newBasket[index], amount:newBasket[index].amount-1}
-        }else {
-            newBasket.splice(index, 1)
+      if (index >= 0) {
+        if (newBasket[index].amount > 1) {
+          newBasket[index] = {
+            ...newBasket[index],
+            amount: newBasket[index].amount - 1,
+          };
+        } else {
+          newBasket.splice(index, 1);
         }
-    }
-        return {...state,
-        basket: newBasket
-       }
-    
+      }
+      return { ...state, basket: newBasket };
+
+    case Type.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
 
     default:
       return state;
