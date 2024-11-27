0b07ec32e61f6f60c8da77cbb592178acc919d77
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-11-27 11:58:18 -0500

    Payment page UI and client-side payment functionality

--- functions/index.js
+++ functions/index.js
@@ -18,14 +18,18 @@ app.get("/", (req, res) => {
 });
 
 app.post("/payment/create", async (req, res) => {
-  const total = req.query.total;
+  const total = parseInt(req.query.total);
   if (total > 0) {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: total,
       currency: "usd",
     });
     console.log(paymentIntent);
-    res.status(201).json({ clientSecret: paymentIntent.clientSecret });
+
+    console.log("Payment Intent response:", {
+      clientSecret: paymentIntent.client_secret,
+    });
+    res.status(201).json({ clientSecret: paymentIntent.client_secret });
   } else {
     res.status(403).json("total must be greater than 0");
   }
