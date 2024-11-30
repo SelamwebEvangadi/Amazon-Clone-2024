0366db0d16f6622db389d04dd52c10bf9f8a3e71
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-11-29 23:38:50 -0500

    firebase deployment BaseURL added

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -4,7 +4,8 @@ const axiosInstance = axios.create({
   //local instance of firebase functions
   // baseURL: "http://127.0.0.1:5001/clone-2024-a9a9c/us-central1/api",
 
-  //deployed version of firebase function --not yet deployed
+  //deployedgit version of firebase function
+  // baseURL= "https://api-gbm55klrjq-uc.a.run.app/"
 
   //deployed version of amazon server on render.com
   baseURL: "https://amazon-api-deploy-2rzn.onrender.com",
